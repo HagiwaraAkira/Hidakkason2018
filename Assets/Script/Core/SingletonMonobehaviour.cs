c41c3e16445b2bf1b0cc2ea8c16c90f769dbefbb
hagiwara_akira <hagiwara_akira@griphone.co.jp>
2018-02-19 22:42:59 +0900

    first commit

---
+++ Assets/Script/Core/SingletonMonobehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour{
+
+	private static T _instance;
+	public static T Instance
+	{
+		get{
+			if (_instance == null) {
+				var t = typeof(T);
+
+				_instance = (T)FindObjectOfType (t);
+				if (_instance == null) {
+					var go = new GameObject(typeof(T).ToString());
+					_instance = go.AddComponent<T>();
+					DontDestroyOnLoad(go);
+				}
+			}
+
+			return _instance;
+		}
+	}
+
+	protected virtual void Awake(){
+		// 他のゲームオブジェクトにアタッチされているか調べる
+		// アタッチされている場合は破棄する。
+		CheckInstance();
+	}
+
+	private void CheckInstance(){
+		if (_instance == null)
+		{
+			_instance = this as T;
+			return;
+		} else if (Instance == this)
+		{
+			return;
+		}
+		Destroy (this);
+		return;
+	}
+}
---
+++ Assets/Script/SceneDirector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDirector : SingletonMonoBehaviour<SceneDirector> {
+    public void Move(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+//        Debug.Log("hogehoge");
+    }
+}
